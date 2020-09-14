7d7ebb6574a1470f854e75aa4449c97ce7c2b2ed
Syed Faateh Sultan Kazmi <faatehsultan@gmail.com>
2020-09-15 00:02:47 +0500

    Launched Windows Desktop App (x64 and x86)

---
+++ desktop-app/app/assets/js/particles-bg/app.js
@@ -0,0 +1,4 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', './assets/js/particles-bg/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
